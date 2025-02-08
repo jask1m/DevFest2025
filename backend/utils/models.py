01cc537dec373271e5a67acab4398d95c7431fc6
jask1m <jkim94960@gmail.com>
2025-02-08 17:25:24 -0500

    start schema for threat detection state

--- backend/utils/models.py
+++ backend/utils/models.py
@@ -7,10 +7,19 @@ class Criteria(BaseModel):
   criteria: dict[str, Any]
 
 class GraphState(BaseModel):
-    description: str
-    existing_criteria: List[Criteria] 
-    selected_criteria: Optional[str] = None
-    sent_from: Optional[str] = None
-    feedback: Optional[str] = None
-    approved: bool = False
+  description: str
+  existing_criteria: List[Criteria] 
+  selected_criteria: Optional[str] = None
+  sent_from: Optional[str] = None
+  feedback: Optional[str] = None
+  approved: bool = False
 
+class ParallelState(BaseModel):
+  criteria: Dict[str, Any]
+  packet: Dict[str, Any] # todo fix schema
+
+  # msg for each agent node
+  payload_msg: str
+
+  threat_detected: bool
+  feedback: str
