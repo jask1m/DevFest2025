6333c26371950718ddacfecedb3f261c34872099
jask1m <jkim94960@gmail.com>
2025-02-08 14:06:26 -0500

    some boilerplate for agents

---
+++ backend/agents/new_criteria_agent.py
@@ -0,0 +1,5 @@
+from utils.models import GraphState
+from langgraph.types import Command
+
+def new_criteria_agent(state: GraphState) -> Command:
+  pass
---
+++ backend/agents/qa_agent.py
@@ -0,0 +1,5 @@
+from utils.models import GraphState
+from langgraph.types import Command
+
+def qa_agent(state: GraphState) -> Command:
+  pass
